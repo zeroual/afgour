e6580579600fd55c2fa4ec39cd01224c18c92951
zeroual <abid1991@hotmail.fr>
2016-07-27 14:48:46 +0200

    change messages to french language

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,10 +1,52 @@
 'use strict';
 
 angular.module('afgourApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, $timeout) {
 
         Principal.identity().then(function (account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $scope.demoMessages = [];
+
+        var $messages = $('.messages');
+
+        $timeout(function () {
+            $scope.demoMessages.push(
+                {
+                    text: 'Salut ! :)',
+                    side: 'left'
+                });
+            $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
+        }, 1000);
+
+        $timeout(function () {
+            $scope.demoMessages.push(
+                {
+                    text: 'Hi ! ça va ?',
+                    side: 'right'
+                });
+            $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
+        }, 2000);
+
+        $timeout(function () {
+            $scope.demoMessages.push(
+                {
+                    text: "Oui très bien , merci!",
+                    side: 'left'
+                });
+            $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
+        }, 3000);
+
+
+
+        $timeout(function () {
+            $scope.demoMessages.push(
+                {
+                    text: "hum, je suis curieux de savoir c ki :D ",
+                    side: 'right'
+                });
+            $messages.animate({scrollTop: $messages.prop('scrollHeight')}, 300);
+        }, 4000);
     });
