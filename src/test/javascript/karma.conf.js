ee6ea39a53c7d9867f7883eb4c2da213bd5ce199
zeroual <abid1991@hotmail.fr>
2016-07-28 10:26:45 +0200

    fix spelling error

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function (config) {
             'main/webapp/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
             'main/webapp/bower_components/sockjs-client/dist/sockjs.js',
             'main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
+            'main/webapp/bower_components/ngToast/dist/ngToast.js',
             'main/webapp/bower_components/angular-mocks/angular-mocks.js',
             // endbower
             'main/webapp/scripts/app/app.js',
